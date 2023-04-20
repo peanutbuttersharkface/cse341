1d180d1d66cd0049e2493d889d05f9fce7aebd4a
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-04-20 11:55:49 -0600

    fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-displayJoke = (req, res) => {
+displayName = (req, res) => {
     const data =
       'How did the telephone propose to his girlfriend? ...he gave her a ring.';
     res.status(200).send(data);
