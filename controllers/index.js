9fd6840d60e3268c31c790be6d4511ef578e0804
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-04-22 19:10:36 -0600

    deleted and restarted

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-displayName = (req, res) => {
-    const data =
-      'How did the telephone propose to his girlfriend? ...he gave her a ring.';
-    res.status(200).send(data);
-  };
-  
-  module.exports = {
-    displayJoke,
-  };
